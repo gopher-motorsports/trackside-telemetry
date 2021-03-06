a70b79d7e5b8d8f3fcbfc54932b29531c669aad2
trackside <king1266@umn.edu>
2021-03-05 23:05:04 -0600

    TracksideLogger API created

---
+++ Vehicle-Telemetry-Module-Software/driver.py
@@ -0,0 +1,26 @@
+import TracksideLogger as tl
+
+logr = tl.TracksideLogger()
+
+def print_formatted(string):
+    white = '\33[107m'
+    black = '\33[30m'
+    bold = '\33[1m'
+    str1 = white + black + bold + string +'\33[0m'
+    print(str1)
+
+while True:
+    try:
+        frame = logr.read()
+        
+        print("Recieved Data!")
+        print(frame)
+        print("\n")
+        
+    except:
+        print("\n")
+        print_formatted("SHUTTING DOWN INTERNALS...")
+        
+        del logr
+        break
+    
