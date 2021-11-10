defb1b81ef1b05519877e868a5d093d21e801592
sriram nutulapati <nutul001@umn.edu>
2021-11-10 01:59:15 -0600

    utils.py function

--- trackside/utils.py
+++ trackside/utils.py
@@ -2,6 +2,12 @@
 Utility functions for trackside-telemetry-software package
 Gopher Motorsports 2021
 '''
+import yaml
+
+def yaml_loader(filepath):
+    with open(filepath, "r") as file_descriptor:
+        data = yaml.load(file_descriptor)
+    return data
 
 def parse_packet(bytes):
     """
@@ -20,5 +26,19 @@ def parse_packet(bytes):
         function and it must complete in less than 0.001 of a second.
     """
 
+    startBit = bytes[0:2]
 
-    return 
+    if startBit == b'7E':
+        name = bytes[2:6]
+        time = bytes[6:14]
+        value = bytes[14:]
+        result = {}
+        filepath = "can_tester.yaml"
+        data = yaml_loader(filepath)
+        dic = data['parameters']
+        for param, info in dic.items():
+            if info['id'] == int(name, 16):
+                result.update({info['human_readable_name']: value})
+        return result
+
+    return {}
