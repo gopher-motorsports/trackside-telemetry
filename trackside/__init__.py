4ddae1a7eb0721bf8f6e2f72ee44b93ec231f447
Anton King <78412634+antonsking@users.noreply.github.com>
2022-03-03 01:49:32 -0600

    Update __init__.py

--- trackside/__init__.py
+++ trackside/__init__.py
@@ -4,6 +4,9 @@ import subprocess
 import sys
 import pathlib
 
+here = str(pathlib.Path(__file__).absolute())
+sys.path.append(here[:-11])
+
 from .DLMSimulator import *
 from .utils import *
 from .reciever import *
