67ef3b0e8aa86c46df5738900034da3ab56c114e
Anton King <78412634+antonsking@users.noreply.github.com>
2022-03-03 01:53:02 -0600

    Update sender.py

--- trackside/sender.py
+++ trackside/sender.py
@@ -1,40 +1,31 @@
-#!/usr/bin/env python
-
-
-## DEMONSTRATION PURPOSES ONLY  - SIMULATE CAR IN MOTION
-## XBEE MUST BE CONNECTED TO SERIAL PORT:
-port = '/dev/tty.usbserial-AK05ZIWP'
-speed = 9600
-
-
+#!/usr/bin/env python3
 
 from DLMSimulator import DLM 
 import serial
 import datetime
 import time
 
-ser = serial.Serial(port,speed)
-sim = DLM()
-prnt = 0
 
-while True:
+if __name__ == "__main__":
+    port = '/dev/tty.usbserial-AK05ZIWP'
+    speed = 9600
+    ser = serial.Serial(port,speed)
+    sim = DLM()
+    prnt = 0
 
-    try:
-        sim.run()
-        packet = sim.data
+    while True:
+        try:
+            sim.run()
+            packet = sim.data
+            ser.write(packet)
+            if prnt == 10:
+                prnt = 0
+                print( ('\33[0m' + "Logged point at: " + '\33[33m') + datetime.datetime.now().strftime("%H:%M:%S") + "\n")
+            else:
+                prnt += 1
+            time.sleep(0.1)
 
-        ser.write(packet)
+        except KeyboardInterrupt:
+            break
 
-        if prnt == 10:
-            prnt = 0
-            print( ('\33[0m' + "Logged point at: " + '\33[33m') + datetime.datetime.now().strftime("%H:%M:%S") + "\n")
-        else:
-            prnt += 1
-
-        time.sleep(0.1)
-
-    except KeyboardInterrupt:
-
-        break
-
-ser.close()
+    ser.close()
