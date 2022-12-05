f5b83cdacdde3fcb99e6c4f3d965fcbf85af9c7e
phantom660 <beratamojit660@gmail.com>
2022-12-05 17:41:32 -0600

    Update reader.py
    Uploads .gdat to influx (hopefully) - Jacob and Tamojit

--- trackside/reader.py
+++ trackside/reader.py
@@ -1,5 +1,7 @@
 from utils import *
 import time
+# import csv
+import InfluxWriter as iw
 
 here = str(pathlib.Path(__file__).absolute())
 filepath = here[:-10] + os.path.join("\\data","sensors.yaml")
@@ -13,7 +15,9 @@ with open("dlm_data_20210413_004735.gdat", "rb") as f:
     start = False
     while (byte := f.read(1)):
         if(byte.hex() == '7e'):
-            print(parse_packet(packet, variable))
+            # print(parse_packet(packet, variable))
+            wrtr = iw.InfluxWriter()
+            wrtr.write([parse_packet(packet, variable)])
             time.sleep(0.1)
             packet = b''
             start = True
