5e13a42e17167e4fbdb8d21355fd0d0649167158
phantom660 <beratamojit660@gmail.com>
2023-01-24 18:18:22 -0600

    Update reader.py

--- trackside/reader.py
+++ trackside/reader.py
@@ -1,24 +1,5 @@
 from utils import *
 import time
-
-here = str(pathlib.Path(__file__).absolute())
-filepath = here[:-10] + os.path.join("\\data","sensors.yaml")
-#global variable
-file_descriptor = open(filepath, "r")  
-variable = yaml.load(file_descriptor, yaml.FullLoader)
-
-# f = open("dlm_data_20210413_004735.gdat", "rb")
-with open("dlm_data_20210413_004735.gdat", "rb") as f:
-    packet = b''
-    start = False
-        if(byte.hex() == '7e'):
-            print(parse_packet(packet, variable))
-            time.sleep(0.1)
-            packet = b''
-            start = True
-        elif start:
-            packet += byte
-        # Do stuff with byte.
 import InfluxWriter as iw
 
 here = str(pathlib.Path(__file__).absolute())
